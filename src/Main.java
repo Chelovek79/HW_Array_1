ba223929b139e8dfc36657a1b8f395e33126d0b8
Vit <privit@rambler.ru>
2022-11-29 00:39:46 +0300

    Задания 1-4 выполнены. Отредактированны. :)))

--- src/Main.java
+++ src/Main.java
@@ -5,66 +5,61 @@ public class Main {
         int [] intArr = new int [] {1, 2, 3};
         double [] doubleArr = {1.57, 7.654, 9.986};
         String [] stringsArr = {"яйца", "молоко", "мука", "соль", "сахар", "вода", "растительное масло"};
+        int n = 0; // Длина массива;
 
 // Задача 2.
         for (int i = 0; i < 3; i++) {
-             switch (i) {
-                case 0 :
-                    for (int j=0; j < intArr.length; j++) {
-                        if (j < intArr.length - 1) {
-                            System.out.print(intArr[j] + ", ");
-                        } else System.out.print(intArr[j] + ".");
-                    }
-                    System.out.println("");
-                break;
-                case 1 :
-                    for (int j=0; j < doubleArr.length; j++) {
-                        if (j < doubleArr.length - 1) {
-                            System.out.print(doubleArr[j] + ", ");
-                        } else System.out.print(doubleArr[j] + ".");
-                    }
-                    System.out.println("");
-                break;
-                case 2 :
-                    for (int j=0; j < stringsArr.length; j++) {
-                        if (j < stringsArr.length - 1) {
-                            System.out.print(stringsArr[j] + ", ");
-                        } else System.out.print(stringsArr[j] + ".");
-                    }
-                    System.out.println("");
-                break;
+            int j = 0;
+            do {
+                switch (i) {
+                    case 0:
+                        n = intArr.length;
+                        System.out.print(intArr[j]);
+                        break;
+                    case 1:
+                        n = doubleArr.length;
+                        System.out.print(doubleArr[j]);
+                        break;
+                    case 2:
+                        n = stringsArr.length;
+                        System.out.print(stringsArr[j]);
+                        break;
+                }
+                if (j < n - 1) {
+                    System.out.print(", ");
+                }
+                j++;
             }
+            while (j < n);
+            System.out.println("");
         }
         System.out.println("--------------------------------------------");
 
 // Задача 3.
         for (int i = 0; i < 3; i++) {
-            switch (i) {
-                case 0 :
-                    for (int j = intArr.length - 1; j >= 0; j--) {
-                        if (j > 0) {
-                            System.out.print(intArr[j] + ", ");
-                        } else System.out.print(intArr[j] + ".");
-                    }
-                    System.out.println("");
-                    break;
-                case 1 :
-                    for (int j = doubleArr.length - 1; j >=0; j--) {
-                        if (j > 0) {
-                            System.out.print(doubleArr[j] + ", ");
-                        } else System.out.print(doubleArr[j] + ".");
-                    }
-                    System.out.println("");
-                    break;
-                case 2 :
-                    for (int j = stringsArr.length - 1; j >= 0; j--) {
-                        if (j > 0) {
-                            System.out.print(stringsArr[j] + ", ");
-                        } else System.out.print(stringsArr[j] + ".");
-                    }
-                    System.out.println("");
-                    break;
+            int j = 1;
+            do {
+                switch (i) {
+                    case 0:
+                        n = intArr.length;
+                        System.out.print(intArr[n-j]);
+                        break;
+                    case 1:
+                        n = doubleArr.length;
+                        System.out.print(doubleArr[n-j]);
+                        break;
+                    case 2:
+                        n = stringsArr.length;
+                        System.out.print(stringsArr[n-j]);
+                        break;
+                }
+                if (j < n) {
+                    System.out.print(", ");
+                }
+                j++;
             }
+            while (j <= n);
+            System.out.println("");
         }
         System.out.println("--------------------------------------------");
 
@@ -73,9 +68,10 @@ public class Main {
             if (intArr[i] % 2 != 0) {
                 intArr [i] = intArr[i] + 1;
             }
+            System.out.print(intArr[i]);
             if (i < intArr.length - 1) {
-                System.out.print(intArr[i] + ", ");
-            } else System.out.print(intArr[i] + ".");
+                System.out.print(", ");
+            }
         }
     }
 }
